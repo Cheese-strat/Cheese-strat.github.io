5b8a48872d73298974d53408642cd94a2d9d5683
Cheese-strat <46002480+Cheese-strat@users.noreply.github.com>
2026-02-12 21:17:52 +0800

    testing css and random points

--- index.js
+++ index.js
@@ -10,8 +10,36 @@ document.getElementsByClassName("image-container")[0].appendChild(span)
 var obj = [
     {
         binnum: 1,
-        top:50,
-        left: 50,
+        top:20,
+        left: 25,
+        description:"",
+        link: undefined
+    },
+    {
+        binnum: 2,
+        top:80,
+        left: 10,
+        description:"",
+        link: undefined
+    },
+    {
+        binnum: 3,
+        top:38,
+        left: 48,
+        description:"",
+        link: undefined
+    },
+    {
+        binnum: 4,
+        top:56,
+        left: 56,
+        description:"",
+        link: undefined
+    },
+    {
+        binnum: 5,
+        top:12,
+        left: 56,
         description:"",
         link: undefined
     }
